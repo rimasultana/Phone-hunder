0995d2279b38e9063d517f88fa2c6404bd9f1331
rimasultana <rimasultana20225@gmail.com>
2024-10-05 23:13:46 +0600

    card and time done

--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
-const loadAllPhones = async () => {
-  console.log("wow 3 second gone");
-  document.getElementById("spinner").style.display = "none";
+const loadAllPhones = async (status, brandName) => {
+    console.log("wow 3 seconds gone");
+    
+   
+    document.getElementById("spinner").style.display = "none";
+  
+    const response = await fetch(
+      "https://openapi.programming-hero.com/api/phones?search=iphone"
+    );
+    const data = await response.json();
+  
+if(status){
+    displayAllPhone(data.data)
+}else{
+    displayAllPhone(data.data.slice(0,6))
+}
 
-  const response = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
-  );
-  const data = await response.json();
-  displayAllPhone(data.data);
-};
+}
+
 
 const displayAllPhone = (phones) =>{
     console.log(phones);
 }
+
+
+
+ const handleShowAll = () =>{
+    loadAllPhones(true)
+ }
 
 const handleSearch = () => {
   document.getElementById("spinner").style.display = "block";
